aac4cb6655e139bf58fb6c2927654a85d50a50ab
Lautaro <lautaroamarchetti@gmail.com>
2022-06-09 16:39:46 -0300

    modificando position en elementros padres style.css

--- js/about.js
+++ js/about.js
@@ -1,10 +1,10 @@
 const headerAbout = document.querySelector("header")
-const logo = document.getElementById("logo")
-const barras= document.getElementById("barras")
+const logoAbout = document.getElementById("logo")
+const barrasAbout= document.getElementById("barras")
 
 
-barras.onload = barras.style.transform = "rotate(0.5turn)"
-logo.onload = logo.style.transform = "scale(1)"
+barrasAbout.onload = barrasAbout.style.transform = "rotate(0.5turn)"
+logoAbout.onload = logoAbout.style.transform = "scale(1)"
 headerAbout.onload = headerAbout.style.opacity = "1"
 
 
