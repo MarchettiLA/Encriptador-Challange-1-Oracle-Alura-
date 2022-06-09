4dd964cf3da2f3694c92c7bc14570b3e14ee07a5
Lautaro <lautaroamarchetti@gmail.com>
2022-06-09 20:20:34 -0300

    resolviendo problemas staged file y revert

--- js/about.js
+++ js/about.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const headerAbout = document.querySelector("header")
 const logoAbout = document.getElementById("logo")
 const barrasAbout= document.getElementById("barras")
@@ -8,6 +9,8 @@ barrasAbout.onload = barrasAbout.style.transform = "rotate(0.5turn)"
 logoAbout.onload = logoAbout.style.transform = "scale(1)"
 headerAbout.onload = headerAbout.style.opacity = "1"
 =======
+=======
+>>>>>>> parent of 6d0070e (reparando error constatante header about)
 const header = document.querySelector("header")
 const logo = document.getElementById("logo")
 const barras= document.getElementById("barras")
@@ -16,6 +19,9 @@ const barras= document.getElementById("barras")
 barras.onload = barras.style.transform = "rotate(0.5turn)"
 logo.onload = logo.style.transform = "scale(1)"
 header.onload = header.style.opacity = "1"
+<<<<<<< HEAD
+>>>>>>> parent of 6d0070e (reparando error constatante header about)
+=======
 >>>>>>> parent of 6d0070e (reparando error constatante header about)
 
 
